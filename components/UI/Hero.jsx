8728d4e4d3aaf9658b7169be146d82f6096fb8d2
mozammil0218 <44997979+mozammilrja@users.noreply.github.com>
2022-11-28 00:03:03 +0530

    entire app

---
+++ components/UI/Hero.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import SectionSubtitle from "./SectionSubtitle";
+import { Container, Row, Col } from "reactstrap";
+import Image from "next/image";
+import Link from "next/link";
+import heroImg from "../../public/images/hero.jpg";
+import classes from "../../styles/hero.module.css";
+
+const Hero = () => {
+  return (
+    <section className={`${classes.hero}`}>
+      <Container>
+        <Row>
+          {/* ========== hero content ============= */}
+          <Col lg="6" md="6">
+            <div className={`${classes.hero__content}`}>
+              <SectionSubtitle subtitle="Hello" />
+              <h2 className="mt-3 mb-3">I&apos;m Mozammil Raja</h2>
+              <h5 className="mb-4">Frontend Developer</h5>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Impedit rem sint ipsa cumque. Atque rem vel iusto impedit omnis
+                quos!
+              </p>
+              <div className="mt-5">
+                <button className="primary__btn">
+                  <Link href="#">Hire me</Link>
+                </button>
+
+                <button className="secondary__btn">
+                  <a href="https://drive.google.com/file/d/1FQ46uRvW5zqAWt32h2InU3WRvLRQj9Hu/view?usp=sharing" target="_blank">Download CV</a>
+                </button>
+              </div>
+            </div>
+          </Col>
+
+          {/* ========== hero img ============= */}
+          <Col lg="6" md="6">
+            <div className={`${classes.hero__img} text-end`}>
+              <Image alt="hero-image" src="https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80" width="400" height="400" />
+
+              <div className={`${classes.hero__skills}`}>
+                <h6>Skills</h6>
+                <span>
+                  <i className="ri-bar-chart-line"></i>
+                </span>
+                <span>
+                  <i className="ri-bar-chart-line"></i>
+                </span>
+                <span>
+                  <i className="ri-bar-chart-line"></i>
+                </span>
+                <span>
+                  <i className="ri-bar-chart-line"></i>
+                </span>
+                <span>
+                  <i className="ri-bar-chart-line"></i>
+                </span>
+                <span>
+                  <i className="ri-bar-chart-line"></i>
+                </span>
+              </div>
+
+              <div
+                className={`${classes.hero__experience} d-flex align-items-center gap-3`}
+              >
+                <span>
+                  <i className="ri-lightbulb-flash-line"></i>
+                </span>
+                <div className="bg-transparent">
+                  <h6>Experience</h6>
+                  <h5 className="mb-0">1 Years</h5>
+                </div>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default Hero;
