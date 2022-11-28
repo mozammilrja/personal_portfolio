1188943fb43755e9bae13fe3ef04ec4a459de25b
mozammil0218 <44997979+mozammilrja@users.noreply.github.com>
2022-11-28 20:58:46 +0530

    add form api

--- components/UI/Form.jsx
+++ components/UI/Form.jsx
@@ -1,24 +1,55 @@
-import React from "react";
+import React,{useState} from "react";
 import classes from "../../styles/form.module.css";
+import { Spinner } from 'reactstrap';
+import Swal from 'sweetalert2';
+import emailjs from 'emailjs-com';
+
 
 const Form = () => {
-  const submitHandler = () => {
+
+  const SERVICE_ID = "service_wc2oq0m"
+  const TEMPLATE_ID = "template_91rdw04";
+  const USER_ID = "LnfHIMY3JdphyZVux";
+  const [isLoading, setIsLoading] = useState(false);
+
+  const submitHandler = (e) => {
     e.preventDefault();
+    setIsLoading(true);
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID)
+      .then((result) => {
+        setIsLoading(false) 
+        console.log(result.text);
+        Swal.fire({
+          icon: "success",
+          title: 'Message Sent Successfully'
+        })
+      }, (error) => {
+        console.log(error.text);
+        Swal.fire({
+          icon: 'error',
+          title: 'Ooops, something went wrong',
+          text: error.text,
+        })
+      });
+    e.target.reset()
   };
   return (
     <form className={`${classes.form}`} onSubmit={submitHandler}>
       <div className={`${classes.form__group}`}>
-        <input type="text" placeholder="Your Name" required />
+        <input type="text" placeholder="Your Name" required name='user_name'/>
       </div>
       <div className={`${classes.form__group}`}>
-        <input type="email" placeholder="Email Address" required />
+        <input type="email" placeholder="Email Address" required name='user_email' />
       </div>
       <div className={`${classes.form__group}`}>
-        <textarea type="text" rows={5} placeholder="Message" required />
+        <textarea type="text" rows={5} placeholder="Message" required name='user_message'/>
       </div>
-
       <button className="primary__btn" type="submit">
-        Send
+        {!isLoading ? "Send" : <> <Spinner size="sm">
+        </Spinner>
+          <span className="loadin-button">
+           Loading...
+          </span></>}
       </button>
     </form>
   );
